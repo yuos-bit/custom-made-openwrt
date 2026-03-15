04b9621c40fff74441cead3c0430445f36bbccf8
小渔 <864529498@qq.com>
2026-03-15 17:51:40 +0800

    更新主题

--- openwrt21.02-2.sh
+++ openwrt21.02-2.sh
@@ -56,7 +56,13 @@ echo "DISTRIB_DESCRIPTION='yuos Build @ ${BUILD_STRING}'" >> package/base-files/
 sed -i '/luciversion/d' feeds/luci/modules/luci-base/luasrc/version.lua
 echo "luciversion = '${BUILD_STRING}'" >> feeds/luci/modules/luci-base/luasrc/version.lua
 
+# 最新主题
+find feeds/luci -type d -name "luci-theme-bootstrap" -exec rm -rf {} +
+rm -rf package/yuos/luci-theme-bootstrap
+cp -rf $GITHUB_WORKSPACE/patchs/5.4/themes $GITHUB_WORKSPACE/package/themes
+
 #升级golang
 rm -rf feeds/packages/lang/golang
 find . -type d -name "golang" -prune -exec rm -rf {} \;
 git clone https://github.com/sbwml/packages_lang_golang -b 25.x feeds/packages/lang/golang
+
