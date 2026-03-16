fbddcb850b53a703275e52d0b934f7657edb212c
小渔 <864529498@qq.com>
2026-03-16 22:12:34 +0800

    修改配置

--- openwrt21.02-1.sh
+++ openwrt21.02-1.sh
@@ -48,10 +48,10 @@ cp -rf $GITHUB_WORKSPACE/patchs/5.4/ZTE/image/mt7621.mk $GITHUB_WORKSPACE/immort
 # cp -rf $GITHUB_WORKSPACE/patchs/5.4/mt76x8/02_network $GITHUB_WORKSPACE/openwrt/target/linux/ramips/mt76x8/base-files/etc/board.d/02_network
 
 #更改软件源
-# sed -i 's|https://git.openwrt.org/feed/packages.git;openwrt-21.02|https://github.com/immortalwrt/packages.git;openwrt-21.02|' feeds.conf.default
-# sed -i 's|https://git.openwrt.org/project/luci.git;openwrt-21.02|https://github.com/immortalwrt/luci.git;openwrt-21.02|' feeds.conf.default
-# sed -i 's|https://git.openwrt.org/feed/routing.git;openwrt-21.02|https://github.com/openwrt/routing.git;openwrt-21.02|' feeds.conf.default
-# sed -i 's|https://git.openwrt.org/feed/telephony.git;openwrt-21.02|https://github.com/openwrt/telephony.git;openwrt-21.02|' feeds.conf.default
+sed -i 's|https://git.openwrt.org/feed/packages.git;openwrt-21.02|https://github.com/immortalwrt/packages.git;openwrt-21.02|' feeds.conf.default
+sed -i 's|https://git.openwrt.org/project/luci.git;openwrt-21.02|https://github.com/immortalwrt/luci.git;openwrt-21.02|' feeds.conf.default
+sed -i 's|https://git.openwrt.org/feed/routing.git;openwrt-21.02|https://github.com/openwrt/routing.git;openwrt-21.02|' feeds.conf.default
+sed -i 's|https://git.openwrt.org/feed/telephony.git;openwrt-21.02|https://github.com/openwrt/telephony.git;openwrt-21.02|' feeds.conf.default
 
 # 增加软件包
 #sed -i 's#github.com/immortalwrt/packages.git;openwrt-21.02#github.com/yuos-bit/other.git;immortalwrt-packages-21.02#' feeds.conf.default
