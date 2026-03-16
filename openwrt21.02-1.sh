05ec5f0afb79e7b03c18b35d231b5e6faca0cdf7
小渔 <864529498@qq.com>
2026-03-16 23:17:51 +0800

    纠正源

--- openwrt21.02-1.sh
+++ openwrt21.02-1.sh
@@ -38,9 +38,9 @@
 # cp -rf $GITHUB_WORKSPACE/patchs/xiaomi_mi-router/mt7620/path/platform.sh $GITHUB_WORKSPACE/openwrt/target/linux/ramips/mt7620/base-files/lib/upgrade/platform.sh
 # MT7621
 # cp -rf $GITHUB_WORKSPACE/patchs/xiaomi_mi-router/mt7621_xiaomi_mi-router-4a-gigabit.dts $GITHUB_WORKSPACE/openwrt/target/linux/ramips/dts/mt7621_xiaomi_mi-router-4a-gigabit.dts
-cp -rf $GITHUB_WORKSPACE/patchs/5.4/ZTE/dts/mt7621_zte_e8820v2.dts $GITHUB_WORKSPACE/immortalwrt/target/linux/ramips/dts/mt7621_zte_e8820v2.dts
-cp -rf $GITHUB_WORKSPACE/patchs/5.4/ZTE/board.d/01_leds $GITHUB_WORKSPACE/immortalwrt/target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
-cp -rf $GITHUB_WORKSPACE/patchs/5.4/ZTE/image/mt7621.mk $GITHUB_WORKSPACE/immortalwrt/target/linux/ramips/image/mt7621.mk
+cp -rf $GITHUB_WORKSPACE/patchs/5.4/ZTE/dts/mt7621_zte_e8820v2.dts $GITHUB_WORKSPACE/openwrt/target/linux/ramips/dts/mt7621_zte_e8820v2.dts
+cp -rf $GITHUB_WORKSPACE/patchs/5.4/ZTE/board.d/01_leds $GITHUB_WORKSPACE/openwrt/target/linux/ramips/mt7621/base-files/etc/board.d/01_leds
+cp -rf $GITHUB_WORKSPACE/patchs/5.4/ZTE/image/mt7621.mk $GITHUB_WORKSPACE/openwrt/target/linux/ramips/image/mt7621.mk
 
 # MT76X8
 # cp -rf $GITHUB_WORKSPACE/patchs/5.4/mt76x8/dts/* $GITHUB_WORKSPACE/openwrt/target/linux/ramips/dts/
